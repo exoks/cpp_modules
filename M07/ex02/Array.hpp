100807a6a60d7f85e60e4f5a7b3a9a04746161ee
exoks <exoks.team@gmail.com>
2023-12-05 00:06:19 +0100

    ex02 M07 in progess

--- M07/ex02/Array.hpp
+++ M07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: oezzaou <oezzaou@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 18:55:33 by oezzaou           #+#    #+#             */
-/*   Updated: 2023/12/04 21:04:30 by oezzaou          ###   ########.fr       */
+/*   Updated: 2023/12/05 00:03:54 by oezzaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,24 @@ class	Array
 		T				*arr;
 
 	public:
-		unsigned int	length;
-		Array(void){
-			length = 10;
-			arr = new T[length];
+		int	length;
+		Array(void) : length(10) {
+			this->arr = new T[length];
 		};
-		Array(unsigned int n){
-			length = n;
-			arr = new T[n];
+		Array(unsigned int n) : length((int) n){
+			this->arr = new T[n];
 		};
 		Array(const Array<T> & arr) {
-			arr = new T[arr.length];
-			this = arr;
+			this->arr = new T[arr.length];
+			*this = arr;
 		};
 
 		Array<T> operator=(const Array<T> & arr)
 		{
-			for (int i; i < length && i < arr.length; i++)
-				arr[index] = arr[index];
+			(void) arr;
+			for (int i = 0; i < length && i < arr.length; i++)
+				(*this)[i] = arr[i];
+			return (*this);
 		}
 		
 		int	size(void)
@@ -48,16 +48,17 @@ class	Array
 			return (length);
 		}
 		
-		T&	operator[](int index)
+		T&	operator[](int index) const
 		{
 			try{
-				if (index > length || index < 0)
+				if (index < 0 || index >= length)
 					throw (std::exception());
 				return (arr[index]);
 			}
 			catch (std::exception & e){
 				std::cout << "OutOfBounds" << std::endl;	
 			}
+			return (arr[0]);
 		}
 };
 
--- M07/ex02/main.cpp
+++ M07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oezzaou <oezzaou@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 18:59:14 by oezzaou           #+#    #+#             */
-/*   Updated: 2023/12/04 19:34:42 by oezzaou          ###   ########.fr       */
+/*   Updated: 2023/12/05 00:00:51 by oezzaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,14 @@
 //====< main >==================================================================
 int	main(void)
 {
-	Array<int>	arr(5);
+	Array<int>	arr;
+	Array<int>	arr2(2);
 
+	arr2[0] = 1;
+	arr2[1] = 2;
+	std::cout << "arr.length : " << arr.length << std::endl;
+	std::cout << "arr2.length : " << arr2.length << std::endl;
+
+	arr = arr2;
 	return (EXIT_SUCCESS);
 }
