7a1cc2a45e5a2f4a4677d7b270a351e8f9b3f646
Oussama Ezzaou <oussamaezzaou@gmail.com>
2025-02-25 13:45:21 +0000

    M02/ex02 is cleaned

--- M02/ex02/Fixed.hpp
+++ M02/ex02/Fixed.hpp
@@ -1,23 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: oezzaou <oezzaou@student.1337.ma>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/11 15:31:24 by oezzaou           #+#    #+#             */
-/*   Updated: 2023/08/31 21:24:17 by oezzaou          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+//  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀                                              
+//  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀               𓐓  Fixed.hpp 𓐔           
+//  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀                                              
+//  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀      Dev: oezzaou <oussama.ezzaou@gmail.com> 
+//  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
+//  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
+//  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2023/08/11 15:31:24 by oezzaou
+//  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/02/25 13:42:25 by oezzaou
+//  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
+//  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
+//  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
+//  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
+//  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
+
+//====<[ Include guards: ]>=====================================================
 #ifndef __FIXED_HPP__
 # define __FIXED_HPP__
+
 # include <iostream>
 # include <cmath>
 
+//====<[ Class: Fixed ]>========================================================
 class	Fixed
 {
 	private:
-		int					fixedPointNbr;
+		int								fixedPointNbr;
 		static const int	fracBits;
 
 	public:
@@ -26,23 +32,26 @@ class	Fixed
 		Fixed(float const floatPointNbr);
 		~Fixed();
 		Fixed(const Fixed& fixed);
+
 		Fixed&	operator=(const Fixed& fixed);
-		bool	operator>(const Fixed& fixed) const;
-		bool	operator<(const Fixed& fixed) const;
-		bool	operator<=(const Fixed& fixed);
-		bool	operator>=(const Fixed& fixed);
-		bool	operator==(const Fixed& fixed);
-		bool	operator!=(const Fixed& fixed);
-		Fixed	operator+(const Fixed& fixed);
-		Fixed	operator-(const Fixed& fixed);
-		Fixed	operator*(const Fixed& fixed);
-		Fixed	operator/(const Fixed& fixed);
+		bool		operator>(const Fixed& fixed) const;
+		bool		operator<(const Fixed& fixed) const;
+		bool		operator<=(const Fixed& fixed);
+		bool		operator>=(const Fixed& fixed);
+		bool		operator==(const Fixed& fixed);
+		bool		operator!=(const Fixed& fixed);
+		Fixed		operator+(const Fixed& fixed);
+		Fixed		operator-(const Fixed& fixed);
+		Fixed		operator*(const Fixed& fixed);
+		Fixed		operator/(const Fixed& fixed);
 		Fixed&	operator++(void);
-		Fixed	operator++(int);
-		int		getRawBits(void) const;
-		void	setRawBits(int const raw);
-		int		toInt(void) const;
-		float	toFloat(void) const;
+		Fixed		operator++(int);
+		
+		int			getRawBits(void) const;
+		void		setRawBits(int const raw);
+		int			toInt(void) const;
+		float		toFloat(void) const;
+		
 		static	Fixed& min(Fixed& f1, Fixed& f2);
 		static	Fixed& min(const Fixed& f1, const Fixed& f2);
 		static 	Fixed& max(Fixed& f1, Fixed& f2);
@@ -51,4 +60,5 @@ class	Fixed
 
 std::ostream&	operator<< (std::ostream& os, const Fixed& fixed);
 
-#endif /* __FIXED_HPP__ */
+#endif /*__FIXED_HPP__*/
+//==============================================================================
