8a55d2b8164af246389bbca9c2b440b0cd13459e
Oussama Ezzaou <oussamaezzaou@gmail.com>
2025-02-25 15:11:15 +0000

    M02: ex03 is cleaned

--- M02/ex03/Fixed.cpp
+++ M02/ex03/Fixed.cpp
@@ -1,123 +1,125 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Fixed.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: oezzaou <oezzaou@student.1337.ma>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/11 15:38:18 by oezzaou           #+#    #+#             */
-/*   Updated: 2023/08/31 21:30:17 by oezzaou          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+//  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀                                              
+//  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀               𓐓  Fixed.cpp 𓐔           
+//  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀                                              
+//  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀      Dev: oezzaou <oussama.ezzaou@gmail.com> 
+//  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
+//  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
+//  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2023/08/11 15:38:18 by oezzaou
+//  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/02/25 15:08:38 by oezzaou
+//  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
+//  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
+//  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
+//  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
+//  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
 
 #include "Fixed.hpp"
 
-//=== Static const int fracBits init ===========================================
+//===<[ Static const int fracBits init: ]>======================================
 const int	Fixed::fracBits = 8;
 
-//=== Fixed Default Constructor ================================================
+//===<[ Fixed Default Constructor: ]>===========================================
 Fixed::Fixed() : fixedPointNbr(0)
 {
 }
 
-//=== Fixed Constructor 1 ======================================================
+//===<[ Fixed Constructor 1: ]>=================================================
 Fixed::Fixed(float const floatPointNbr)
 {
 	fixedPointNbr = roundf(floatPointNbr * ((float) (1 << fracBits)));
 }
 
-//=== Fixed Constructor 2 ======================================================
+//===<[ Fixed Constructor 2: ]>=================================================
 Fixed::Fixed(int const intNbr)
 {
 	fixedPointNbr = intNbr << fracBits;
 }
 
-//=== Fixed Copy Constructor ===================================================
+//===<[ Fixed Copy Constructor: ]>==============================================
 Fixed::Fixed(const Fixed &fixed)
 {
 	(*this) = fixed;
 }
 
-//=== Fixed Distructor =========================================================
+//===<[ Fixed Distructor: ]>====================================================
 Fixed::~Fixed()
 {
 }
 
-//=== Fixed Copy Assignment Operator ===========================================
+//===<[ Fixed Copy Assignment Operator: ]>======================================
 Fixed&	Fixed::operator= (const Fixed& fixed)
 {
 	fixedPointNbr = fixed.getRawBits();
 	return (*this);
 }
 
-//=== Fixed > Operator =========================================================
+//===<[ Fixed > Operator: ]>====================================================
 bool	Fixed::operator> (const Fixed& fixed) const
 {
 	return (fixedPointNbr > fixed.getRawBits());
 }
 
-//=== Fixed < Operator =========================================================
+//===<[ Fixed < Operator: ]>====================================================
 bool	Fixed::operator< (const Fixed& fixed) const
 {
 	return (fixedPointNbr < fixed.getRawBits());
 }
 
-//=== Fixed <= Operator =========================================================
+//===<[ Fixed <= Operator: ]>===================================================
 bool	Fixed::operator<= (const Fixed& fixed)
 {
 	return (fixedPointNbr <= fixed.getRawBits());
 }
 
-//=== Fixed >= Operator =========================================================
+//===<[ Fixed >= Operator: ]>===================================================
 bool	Fixed::operator>= (const Fixed& fixed)
 {
 	return (fixedPointNbr >= fixed.getRawBits());
 }
 
-//=== Fixed == Operator =========================================================
+//===<[ Fixed == Operator: ]>===================================================
 bool	Fixed::operator== (const Fixed& fixed)
 {
 	return (fixedPointNbr == fixed.getRawBits());
 }
 
-//=== Fixed != Operator =========================================================
+//===<[ Fixed != Operator: ]>===================================================
 bool	Fixed::operator!= (const Fixed& fixed)
 {
 	return (fixedPointNbr != fixed.getRawBits());
 }
 
-//=== Fixed + Operator =========================================================
+//===<[ Fixed + Operator: ]>====================================================
 Fixed	Fixed::operator+ (const Fixed& fixed)
 {
 	return (Fixed((*this).toFloat() + fixed.toFloat()));
 }
 
-//=== Fixed - Operator =========================================================
+//===<[ Fixed - Operator: ]>====================================================
 Fixed	Fixed::operator- (const Fixed& fixed)
 {
 	return (Fixed((*this).toFloat() - fixed.toFloat()));
 }
 
-//=== Fixed * Operator =========================================================
+//===<[ Fixed * Operator: ]>====================================================
 Fixed	Fixed::operator* (const Fixed& fixed)
 {
 	return (Fixed((*this).toFloat() * fixed.toFloat()));
 }
 
-//=== Fixed / Operator =========================================================
+//===<[ Fixed / Operator: ]>====================================================
 Fixed	Fixed::operator/ (const Fixed& fixed)
 {
 	return (Fixed((*this).toFloat() / fixed.toFloat()));
 }
 
-//=== Fixed Pre-Increment Operator =============================================
+//===<[ Fixed Pre-Increment Operator: ]>========================================
 Fixed&	Fixed::operator++ (void)
 {
 	++fixedPointNbr;
 	return (*this);
 }
 
-//=== Fixed Post-Increment Operator ============================================
+//===<[ Fixed Post-Increment Operator: ]>=======================================
 Fixed	Fixed::operator++ (int)
 {
 	Fixed	old = (*this);
@@ -125,31 +127,31 @@ Fixed	Fixed::operator++ (int)
 	return (old);
 }
 
-//=== getRawBits ===============================================================
+//===<[ getRawBits: ]>==========================================================
 int		Fixed::getRawBits(void) const
 {
 	return (fixedPointNbr);
 }
 
-//=== setRawBits ===============================================================
+//===<[ setRawBits: ]>==========================================================
 void	Fixed::setRawBits(int const raw)
 {
 	fixedPointNbr = raw;
 }
 
-//=== toInt ====================================================================
+//===<[ toInt: ]>===============================================================
 int		Fixed::toInt(void) const
 {
 	return (fixedPointNbr >> fracBits);
 }
 
-//=== toInt ====================================================================
+//===<[ toFloat: ]>=============================================================
 float	Fixed::toFloat(void) const
 {
 	return (fixedPointNbr / ((float)(1 << fracBits)));
 }
 
-//=== Min ======================================================================
+//===<[ Min: ]>=================================================================
 Fixed& Fixed::min(Fixed& f1, Fixed& f2)
 {
 	if (f1.getRawBits() < f2.getRawBits())
@@ -157,7 +159,7 @@ Fixed& Fixed::min(Fixed& f1, Fixed& f2)
 	return (f2);
 }
 
-//=== min const ================================================================
+//===<[ min const: ]>===========================================================
 Fixed& Fixed::min(const Fixed& f1, const Fixed& f2)
 {
 	if (f1.getRawBits() < f2.getRawBits())
@@ -165,7 +167,7 @@ Fixed& Fixed::min(const Fixed& f1, const Fixed& f2)
 	return ((Fixed&) f2);
 }
 
-//=== Max ======================================================================
+//===<[ Max: ]>=================================================================
 Fixed& Fixed::max(Fixed& f1, Fixed& f2)
 {
 	if (f1.getRawBits() > f2.getRawBits())
@@ -173,7 +175,7 @@ Fixed& Fixed::max(Fixed& f1, Fixed& f2)
 	return (f2);
 }
 
-//=== Max const ================================================================
+//===<[ Max const: ]>===========================================================
 Fixed& Fixed::max(const Fixed& f1, const Fixed& f2)
 {
 	if (f1.getRawBits() > f2.getRawBits())
@@ -181,7 +183,7 @@ Fixed& Fixed::max(const Fixed& f1, const Fixed& f2)
 	return ((Fixed&) f2);
 }
 
-//=== Insertion operator =======================================================
+//===<[ Insertion operator: ]>==================================================
 std::ostream& operator<< (std::ostream& os, const Fixed& fixed)
 {
 	return (os << fixed.toFloat());
