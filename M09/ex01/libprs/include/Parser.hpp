9384dd50a413c507da4ecfba1f7b020c870c7998
Oussama Ezzaou <oussamaezzaou@gmail.com>
2025-03-02 09:49:32 +0000

    M09: ex01: libprs:latest is added

---
+++ M09/ex01/libprs/include/Parser.hpp
@@ -0,0 +1,91 @@
+//  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀                                              
+//  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀              𓐓  Parser.hpp 𓐔           
+//  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀                                              
+//  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀      Dev: oezzaou <oussama.ezzaou@gmail.com> 
+//  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
+//  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
+//  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2024/04/02 23:09:19 by oezzaou
+//  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/03/02 09:01:32 by oezzaou
+//  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
+//  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
+//  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
+//  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
+//  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
+
+//====<[ KeyValueParser Class: ]>===============================================
+#ifndef __PARSER_HPP__
+# define __PARSER_HPP__
+
+//====<[ Includes: ]>=======================================
+# include <sstream>
+# include <iostream>
+# include <string>
+# include <fstream>
+# include <map>
+# include "Date.hpp"
+# include "FSM.hpp"
+# include "Exception.hpp"
+
+//====<[ Enum: Type ]>======================================
+typedef enum	e_type
+{
+	POSITIVE,
+	NEGATIVE,
+	MIXED
+}				      Type;
+
+//====<[ Namespace: prs ]>======================================================
+namespace	prs
+{
+//====< Functions >=============================================================
+	std::string   parse(std::string value, std::string sValue);
+	int           parse(int value, std::string sValue);
+	double        parse(double value, std::string sValue);
+
+	std::string   trim(std::string str);
+	std::string   getNextLine(std::istream & stream);
+
+//====< KeyValueParser class >==================================================
+	template <class p1, class p2, char sep = '|'>
+	class	KeyValueParser
+	{
+		private:
+			std::string		fileName;
+
+		public:
+			std::fstream	fileStream;
+
+			KeyValueParser(void);
+			KeyValueParser(std::fstream fileStream);
+			KeyValueParser(std::string fileName);
+			~KeyValueParser(void);
+
+			std::map<p1, p2>  parseFile(void);
+			std::pair<p1, p2> parseNextLine(void);
+			std::string       skipLine(void);
+			bool              eof(void);
+	};
+
+# include "KeyValueParser.tpp"
+
+//====< PairParser class >======================================================
+	template<class Container, class Unit, int type = MIXED, bool sortPair = false>
+	class	PairParser
+	{
+		private:
+			char	**av;
+			int		ac;
+
+		public:
+			PairParser(int ac, char **av);
+			~PairParser(void);
+
+			Container	parseArgs(void);
+	};
+
+# include "PairParser.tpp"
+
+};
+
+#endif /*__PARSER_HPP__*/
+//==============================================================================
